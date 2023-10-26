eb089c1b58c9bb70aa8f7f0506a53830b983a8d0
Satax <144334728+ArturGilimhanovSatax@users.noreply.github.com>
2023-10-26 15:18:39 +0300

    Add files via upload

--- CPerson.py
+++ CPerson.py
@@ -1,19 +1,20 @@
 class CPerson:
+	def __init__(self, surname, name, ptr, num, month, year, gender):
+		self.surname = surname
+		self.name = name
+		self.ptr = ptr
+		self.num = num
+		self.month = month
+		self.year = year
+		self.gender = gender
 
-	def __del__():
+	def __del__(self):
 		print(f'\nКласс CPerson ничтожен')
 
-	def person():
-		surname = input()
-		name = input()
-		ptr = input()
-		num = input()
-		month = input()
-		year = input()
-		gender = input()
+	def person(self):
 
-		print(f'ФИО: {surname} {name} {ptr}\nДата рождения: {num}.{month}.{year}\nПол: {gender}')
+		print(f'ФИО: {self.surname} {self.name} {self.ptr}\nДата рождения: {self.num}.{self.month}.{self.year}\nПол: {self.gender}')
 
-CPerson.person()
-CPerson.__del__()
+John = CPerson('Surn', 'Name', 'Ptr', '12', '02', '2005', 'male')
+John.person()
 del CPerson
